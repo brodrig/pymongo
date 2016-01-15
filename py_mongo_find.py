aa7889e3e45499babcbe5f90dd5f84a7465ff457
brodrig <briancosmorodriguez@gmail.com>
2016-01-14 19:41:28 -0500

    Initial commit, one python script querying MongoDB and enumerating over results.

---
+++ py_mongo_find.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+
+
+client = MongoClient('localhost', 27017)
+
+db = client.zipcodes
+
+collection = db.zips
+
+docs = collection.find(
+    {"city" : {"$in" : ["BUFFALO", "NEW YORK"]}},  # query
+    {"city" : 1,  "state" : 1, "_id" : 1, "pop" : 1}  # projection
+)  # cursor object
+
+# docs = collection.find({})
+
+for i, document in enumerate(docs):
+    print "{}: {}".format(i, document)
